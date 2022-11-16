378b4184e60c107a2e041b643ddd3de3436fc70c
grsarah <118317502+grsarah@users.noreply.github.com>
2022-11-16 14:21:00 +1100

    howdy 1

--- Test.R
+++ Test.R
@@ -1,3 +1,6 @@
 library(tidyr)
 
-df <- 
+library(usethis)
+use_git_config(user.name = "grsarah", user.email = "sarahgreen.au@gmail.com")
+use_github(protocol = 'https', auth_token = Sys.getenv("GITHUB_PAT"))  
+
